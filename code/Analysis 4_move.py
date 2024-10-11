98221f34c3b578e2e431deda115e4fb927f13552
吴嵩阳 <wusongyang@wireless-10-146-231-159.public.utexas.edu>
2024-10-11 15:29:39 -0500

    update data analysis topic

--- code/Analysis 4_move.py
+++ code/Analysis 4_move.py
@@ -1,4 +1,4 @@
 # Distrubution of movie ratings across genres
 
 # Awards vs box office performance
-
+#"study whether the top 250 movies are teenager friendly enough, and different genre's movie's teenager friendly level"
