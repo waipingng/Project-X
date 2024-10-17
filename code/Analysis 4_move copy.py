95aee0d66df98d922f2a94c767810cc56455dce2
吴嵩阳 <wusongyang@wusongyangdeAir.lan>
2024-10-17 04:08:12 -0500

    improve code and add interpretation

--- code/Analysis 4_move copy.py
+++ code/Analysis 4_move copy.py
@@ -10,49 +10,41 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from bs4 import BeautifulSoup
-import json
-with open('imdb_top_movies.json', 'r') as f:
-    data = json.load(f)
 
-df = pd.DataFrame(data)
-
-df.to_csv('movies.csv', index=False, columns=['title', 'url', 'rating', 'rating_count', 'description', 'genre', 'content_rating', 'duration'])
-
-df = pd.read_csv('movies.csv')
-
+df = pd.read_csv('artifacts/movies.csv')
 ########### the average rate of the different genre 
 average_ratings = df.groupby('genre')['rating'].mean().reset_index()
 
 average_ratings.columns = ['genre', 'average_rating']
 
 average_ratings.to_csv('average_ratings_by_genre.csv', index=False)
-########### the probality of different genre's conten rating 
+# ########### the probality of different genre's conten rating 
 
-count_df = df.groupby(['genre', 'content_rating']).size().unstack(fill_value=0)
-
-
-total_counts = count_df.sum(axis=1)
+# count_df = df.groupby(['genre', 'content_rating']).size().unstack(fill_value=0)
 
 
-probability_df = count_df.div(total_counts, axis=0)
+# total_counts = count_df.sum(axis=1)
 
 
-probability_df.columns.name = None  
-probability_df.reset_index(inplace=True)  
-
-probability_df.to_csv('genre_content_rating_probabilities.csv', index=False)
-#
-###### merge two csv
-csv1 = pd.read_csv('average_ratings_by_genre.csv') 
-csv2 = pd.read_csv('genre_content_rating_probabilities.csv')  
-
-merged_df = pd.merge(csv1, csv2, on='genre')
+# probability_df = count_df.div(total_counts, axis=0)
 
 
-sorted_df = merged_df.sort_values(by='average_rating', ascending=False)
+# probability_df.columns.name = None  
+# probability_df.reset_index(inplace=True)  
+
+# probability_df.to_csv('genre_content_rating_probabilities.csv', index=False)
+# #
+# ###### merge two csv
+# csv1 = pd.read_csv('average_ratings_by_genre.csv') 
+# csv2 = pd.read_csv('genre_content_rating_probabilities.csv')  
+
+# merged_df = pd.merge(csv1, csv2, on='genre')
 
 
-sorted_df.to_csv('merge_genre_rating_ct_prob.csv', index=False)
+# sorted_df = merged_df.sort_values(by='average_rating', ascending=False)
+
+
+# sorted_df.to_csv('merge_genre_rating_ct_prob.csv', index=False)
 
 ####### redo 
 #####average 
@@ -73,7 +65,7 @@ average_ratings.columns = ['new_genre', 'average_rating']
 
 average_ratings.to_csv('average_ratings_by_new_genre.csv', index=False)
 
-###########################the content rate by new genre##########
+###########################the content rate by new genre###############
 import pandas as pd
 
 
@@ -105,7 +97,7 @@ probability_df.reset_index(inplace=True)
 
 final_df = pd.merge(average_ratings, probability_df, on='new_genre', how='outer')
 
-final_df.to_csv('contentrating_genre.csv', index=False)
+final_df.to_csv('genre_audience_breadth.csv', index=False)
 
 ################## content rate  by rate ############
 df = pd.read_csv('movies.csv')
@@ -133,15 +125,7 @@ count_df.reset_index(inplace=True)
 count_df.to_csv('new_counting_rating.csv', index=False)
 
 
-# df['rating_category'] = df['rating'].apply(classify_rating)
+df['rating_category'] = df['rating'].apply(classify_rating)
 
-# count_df = df.groupby(['rating_category', 'content_rating']).size().unstack(fill_value=0)
 
-# total_counts = count_df.sum(axis=1)
 
-# probability_df = count_df.div(total_counts, axis=0)
-
-# probability_df.columns.name = None  
-# probability_df.reset_index(inplace=True)
-
-# probability_df.to_csv('content_rating_probabilities_by_rating_category.csv', index=False)
--- code/Analysis genre_audience_breadth_code.py
+++ code/Analysis genre_audience_breadth_code.py
@@ -1,37 +1,38 @@
 # use python code and use movies.csv file we have scraped from the website to get the new csv file which can show the relationship between the genre and its breadth of a movie’s audience .
 #now use the content rating to represent the the breadth of a movie’s audience
-import pandas as pd
+import pandas as pd  
+import json  
 
+# step1: extract the dataframe from the json file , then create the CSV file with specified columns
+with open('imdb_top_movies.json', 'r') as f:
+    data = json.load(f) 
+df = pd.DataFrame(data)
+df.to_csv('artifacts/moviesnew.csv', index=False, columns=['title', 'url', 'rating', 'rating_count', 'description', 'genre', 'content_rating', 'duration'])
 
-# Reading the CSV file from the 'artifacts' directory
-df = pd.read_csv('artifacts/movies.csv')
-
-new_genres = []
-
-# Iterating through each row, splitting the 'genre' column, and creating new records
+# step 2: Read CSV file into a DataFrame
+df = pd.read_csv('moviesnew.csv')
+new_genres = []  
+# step3: create the new genres list
 for index, row in df.iterrows():
     genres = row['genre'].split(', ')  
-    for genre in genres:
-        new_genres.append({'new_genre': genre.strip(), 'rating': row['rating'], 'content_rating': row['content_rating']})
-
-# Creating a new DataFrame from the new genres data
+    for genre in genres: 
+        new_genres.append({'new_genre': genre.strip(), 'content_rating': row['content_rating']})
 new_genres_df = pd.DataFrame(new_genres)
-
-# Grouping by 'new_genre' and 'content_rating' and counting occurrences
+ 
+# step4: Group by 'new_genre' and 'content_rating'
 count_df = new_genres_df.groupby(['new_genre', 'content_rating']).size().unstack(fill_value=0)
 
-# Calculating the total count for each 'new_genre'
+# step5: Calculate the total counts of content rating of each new genre
 total_counts = count_df.sum(axis=1)
 
-# Calculating the proportion of each 'content_rating' within each 'new_genre'
+# step6: Calculate the proportion of each content rating of each new genre
 probability_df = count_df.div(total_counts, axis=0)
 
-# Resetting the index to flatten the table
+# step7: create the new dataframe and save it to the new csv file
 probability_df.columns.name = None  
 probability_df.reset_index(inplace=True)
-
-# Saving the final DataFrame to a CSV file
-count_df.to_csv('artifacts/genre_audience_breadth.csv', index=False)
+probability_df.rename(columns={'index': 'new_genre'}, inplace=True)
+probability_df.to_csv('artifacts/genre_audience_breadth.csv', index=False)
 
 
 
