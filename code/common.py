61b7765c2f3e0d5778246269a9ce5ed2b3db1e3f
王毓晨 <wangyuchen@wireless-10-148-194-134.public.utexas.edu>
2024-10-17 11:13:44 -0500

    revise code

--- code/common.py
+++ code/common.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 url = "https://www.imdb.com/chart/top"
 headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
 }
 
 def get_soup(url, headers):
