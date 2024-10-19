7fb2ea205f7007117bb81281b416b210dd84a138
吴嵩阳 <wusongyang@wireless-10-154-76-217.public.utexas.edu>
2024-10-18 21:44:48 -0500

    update

--- code/genre_audience_breadth_code.py
+++ code/genre_audience_breadth_code.py
@@ -1,8 +1,5 @@
-# use python code and use movies.csv file we have scraped from the website to get the new csv file which can show the relationship between the genre and its breadth of a movie’s audience .
-#now use the content rating to represent the the breadth of a movie’s audience
 import pandas as pd   
 import re
-
 
 # Step 1: Read the CSV file into a DataFrame
 def read_csv_to_dataframe(csv_file):
